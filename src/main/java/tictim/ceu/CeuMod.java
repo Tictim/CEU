b979566384714dcadd657506010bc2f6d711c5c3
Tictim <ksw07149@gmail.com>
2021-03-21 14:42:05 +0900

    1.0.5.2
    Signed-off-by: Tictim <ksw07149@gmail.com>

--- src/main/java/tictim/ceu/CeuMod.java
+++ src/main/java/tictim/ceu/CeuMod.java
@@ -33,7 +33,7 @@ import java.util.List;
 public class CeuMod{
 	public static final String MODID = "ceu";
 	public static final String NAME = "Ceu";
-	public static final String VERSION = "1.0.5.1";
+	public static final String VERSION = "1.0.5.2";
 
 	public static final SimpleNetworkWrapper NET = NetworkRegistry.INSTANCE.newSimpleChannel(MODID);
 
