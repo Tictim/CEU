3c51af92f88e9ac4a44f8fa559709cda05cf9e10
Tictim <ksw07149@gmail.com>
2021-05-12 15:20:34 +0900

    1.0.5.3
    Signed-off-by: Tictim <ksw07149@gmail.com>

--- src/main/java/tictim/ceu/CeuMod.java
+++ src/main/java/tictim/ceu/CeuMod.java
@@ -35,7 +35,7 @@ import java.util.List;
 public class CeuMod{
 	public static final String MODID = "ceu";
 	public static final String NAME = "Ceu";
-	public static final String VERSION = "1.0.5.3-SNAPSHOT";
+	public static final String VERSION = "1.0.5.3";
 
 	public static final Logger LOGGER = LogManager.getLogger(NAME);
 
